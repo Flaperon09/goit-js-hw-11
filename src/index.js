11570c698b7ca5ddb909367d95745a1ab7c53c6f
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-04-01 18:01:14 +0300

    Внешний итератор

--- src/index.js
+++ src/index.js
@@ -27,31 +27,42 @@ function setOutput(evt) {
         .then(response => {
           console.log(response);
             // Добавление разметки загруженных фото
-            galleryOfPhotos.innerHTML = createGallery(response);
+          // galleryOfPhotos.innerHTML = createGallery(response.hits);
+          createGallery(response.hits);
         })
         .catch(error => console.log(error));
 };
 
 // Функция создания разметки галереи
 function createGallery(arr) {
-    // const { hits: [{ webformatURL, largeImageURL, tags, likes, views, comments, downloads }] } = arr;
-    console.log(arr);
-  return arr.map(({ hits: [{ webformatURL, largeImageURL, tags, likes, views, comments, downloads }] }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes: ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${downloads}</b>
-    </p>
-  </div>
-</div>`).join('')
-};
+   
+  // ==== Внешний итератор - начало =================================
+  const iterator = arr[Symbol.iterator]();
+  let result = iterator.next();
 
+  while (!result.done) {
+    const element = result.value;
+    // Выполняемые действия - начало
+    const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = element;    
+    galleryOfPhotos.insertAdjacentHTML("afterbegin", `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${downloads}</b>
+        </p>
+      </div>
+    </div>`);
+    // Выполняемые действия - конец
+    result = iterator.next();
+  }
+  // ==== Внешний итератор - конец =================================
+}
