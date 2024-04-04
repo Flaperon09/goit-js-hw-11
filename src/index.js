bee2d92b96dd8c2563e1113e7f10554187a0d85c
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-04-04 10:24:43 +0300

    Работает MAP

--- src/index.js
+++ src/index.js
@@ -20,49 +20,36 @@ inputOfUser.addEventListener('submit', setOutput);
 
 // Функция обработки нажатия на кнопку Поиск
 function setOutput(evt) {
-    // Запрет действий по-умолчанию
-    evt.preventDefault();
-    // Функция запроса на сервер по введённому слову
-    getPhotos(wordForSearch)
-        .then(response => {
-          console.log(response);
-            // Добавление разметки загруженных фото
-          // galleryOfPhotos.innerHTML = createGallery(response.hits);
-          createGallery(response.hits);
-        })
-        .catch(error => console.log(error));
+  // Запрет действий по-умолчанию
+  evt.preventDefault();
+  // Функция запроса на сервер по введённому слову
+  getPhotos(wordForSearch)
+    .then(response => {
+      console.log(response);
+      // Добавление разметки загруженных фото
+      galleryOfPhotos.innerHTML = createGallery(response.hits);
+      })
+    .catch(error => console.log(error));
 };
 
 // Функция создания разметки галереи
 function createGallery(arr) {
-   
-  // ==== Внешний итератор - начало =================================
-  const iterator = arr[Symbol.iterator]();
-  let result = iterator.next();
-
-  while (!result.done) {
-    const element = result.value;
-    // Выполняемые действия - начало
-    const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = element;    
-    galleryOfPhotos.insertAdjacentHTML("afterbegin", `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes: ${likes}</b>
-        </p>
-        <p class="info-item">
-          <b>Views: ${views}</b>
-        </p>
-        <p class="info-item">
-          <b>Comments: ${comments}</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads: ${downloads}</b>
-        </p>
-      </div>
-    </div>`);
-    // Выполняемые действия - конец
-    result = iterator.next();
-  }
-  // ==== Внешний итератор - конец =================================
+  console.log(arr);
+  return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+       <div class="info">
+         <p class="info-item">
+           <b>Likes: ${likes}</b>
+         </p>
+         <p class="info-item">
+           <b>Views: ${views}</b>
+         </p>
+         <p class="info-item">
+         <b>Comments: ${comments}</b>
+         </p>
+         <p class="info-item">
+           <b>Downloads: ${downloads}</b>
+         </p>
+       </div>
+     </div>`).join(' ');
 }
